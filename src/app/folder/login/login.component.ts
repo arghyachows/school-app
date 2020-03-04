43a1eb0accef8b5e30b6aa8ec4b58d57084a238a
Arghya Chowdhury <arghyachowdhury2610@gmail.com>
2020-03-04 20:36:08 +0530

    Login Validations

--- src/app/folder/login/login.component.ts
+++ src/app/folder/login/login.component.ts
@@ -7,35 +7,45 @@ import { FormBuilder, FormGroup, Validators, ValidationErrors, AbstractControl }
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+
   newUser: boolean = false;
-  name: string;
   registerForm: FormGroup;
+  loginForm: FormGroup;
   showPassword: boolean;
-  logForm(form) {
-    console.log(form.value)
-  }
+
   constructor(fb: FormBuilder) {
+
+    //Form Group for Registration
     this.registerForm = fb.group({
       'username': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(12)])],
       'email': [null, Validators.compose([Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])],
       'password': [null, Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])],
-      'retype_password': [null, Validators.compose([Validators.required, this.matchValues('password')])],
+      'retype_password': [null, Validators.compose([Validators.required, this.matchValues('password')])]
+    })
+
+    //Form Group for Login
+    this.loginForm = fb.group({
+      'username': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(12)])],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(9)])]
     })
   }
 
+
+  ngOnInit() { }
+
+  //Fired on Sign Up button click event
   register(value: any): void {
     console.log('Form submited!')
     console.log(value);
   }
-  ngOnInit() { }
 
-  login(form) {
-    // this.authService.register(form.value).subscribe((res) => {
-    //   this.router.navigateByUrl('home');
-    // });
-    console.log(form.value);
+  //Fired on Login button click event
+  login(value: any): void {
+    console.log('Form submited!')
+    console.log(value);
   }
 
+  //Match typed passwords while registration
   matchValues(
     matchTo: string // name of the control to match to
   ): (AbstractControl) => ValidationErrors | null {
@@ -48,6 +58,7 @@ export class LoginComponent implements OnInit {
     };
   }
 
+  //Toggle show/hide password
   public onPasswordToggle(): void {
     this.showPassword = !this.showPassword;
   }
