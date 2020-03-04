bd11eb6c8a4eae716ac1c46bd2fcdf036e2c1e38
Arghya Chowdhury <arghyachowdhury2610@gmail.com>
2020-03-04 20:49:22 +0530

    Menu Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,34 +13,29 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
+      title: 'News',
+      url: '/folder/News',
+      icon: 'copy'
+    },
+    {
+      title: 'Users',
+      url: '/folder/Users',
+      icon: 'people'
+    },
+    {
+      title: 'Timetable',
+      url: '/folder/Timetable',
+      icon: 'time'
+    },
+    {
+      title: 'Classes',
+      url: '/folder/Classes',
+      icon: 'easel'
+    },
+    {
       title: 'Login',
       url: '/folder/Login',
-      icon: 'mail'
-    },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      icon: 'person'
     }
   ];
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
